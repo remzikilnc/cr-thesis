bde85941b3046b09095b374a2ad75f8a02764436
Remzi Kılınç <remzikilnc46@gmail.com>
2023-04-25 17:24:08 +0300

    Revise frontend authentication and bug fixes

--- resources/js/store/auth/authSlice.jsx
+++ resources/js/store/auth/authSlice.jsx
@@ -4,28 +4,34 @@ import JWTEncode from "jwt-encode";
 import jwt_decode from "jwt-decode";
 import {getTokenFromLocal} from "@/utils/AuthUtils";
 
-export const tokenStore = createSlice({
-    name: 'token',
-    initialState: getTokenFromLocal,
+export const authStore = createSlice({
+    name: 'auth',
+    initialState: {
+        token:getTokenFromLocal,
+        user: null
+    } ,
     reducers: {
         saveToken: (state, action) => {
             try {
                 const data = action.payload;
-                const jwtEncodedData = JWTEncode(data, 'secret');
+                const token = data.token;
+                const jwtEncodedData = JWTEncode(token, 'secret');
                 const encryptedAppState = crypto.AES.encrypt(jwtEncodedData, "crthesisSecret").toString();
-                localStorage.setItem('token', encryptedAppState);
+                localStorage.setItem('cr.personal.token', encryptedAppState);
                 state.token = jwt_decode(jwtEncodedData)
+                state.user = data.user
             } catch (e) {
 
             }
         },
         removeToken: (state) => {
             state.token = null
-            localStorage.removeItem('token')
+            state.user = null
+            localStorage.removeItem('cr.personal.token');
         }
     },
 })
 
-export const {saveToken, removeToken} = tokenStore.actions
+export const {saveToken, removeToken} = authStore.actions
 
-export default tokenStore.reducer
+export default authStore.reducer
